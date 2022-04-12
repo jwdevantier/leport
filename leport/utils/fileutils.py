ac9daccfbedf248b7b906cc025cf3e4c081637f3
Jesper Wendel Devantier <jwd@defmacro.it>
2022-04-13 01:10:31 +0200

    various util fns (user, group lookup etc)

--- leport/utils/fileutils.py
+++ leport/utils/fileutils.py
@@ -1,3 +1,4 @@
+import grp
 import hashlib
 import importlib.util
 from urllib.request import urlretrieve
@@ -5,8 +6,10 @@ from urllib.parse import urlparse
 from typing import Optional, Union, Generator
 from types import ModuleType
 from pathlib import Path
+import pwd
 from contextlib import contextmanager
 import os
+import glob
 import subprocess
 from rich.progress import Progress
 from rich import print
@@ -109,3 +112,100 @@ def user_home() -> Path:
     return Path(os.path.expanduser(
         "~"
         + (os.environ.get("SUDO_USER") or os.environ.get("USER"))))
+
+
+def user_uid(name: str, missing_ok=True) -> Optional[int]:
+    try:
+        return pwd.getpwnam(name).pw_uid
+    except KeyError as e:
+        if missing_ok:
+            return None
+        raise RuntimeError(f"missing user '{name}'") from e
+
+
+def group_info(name: str) -> Optional[grp.struct_group]:
+    """Return group struct, iff group exists.
+
+    Args:
+        name: group name
+
+    Returns:
+        Group struct iff. group exists, None otherwise.
+    """
+    try:
+        return grp.getgrnam(name)
+    except KeyError:
+        return None
+
+
+def group_gid(name: str, missing_ok=True) -> Optional[int]:
+    g = group_info(name)
+    if g:
+        return g.gr_gid
+    if missing_ok:
+        return None
+    raise RuntimeError(f"missing group '{name}'")
+
+
+def current_group() -> grp.struct_group:
+    return grp.getgrgid(os.getegid())
+
+
+_uid_cache = {}
+_gid_cache = {}
+
+
+def stat_set(path: Path,
+         user: Union[str, int] = None,
+         group: Union[str, int] = None,
+         perms: int = None):
+
+    if isinstance(user, str):
+        val = _uid_cache.get(user, None)
+        if val is None:
+            val = user_uid(user)
+            if val is None:
+                # TODO: nicer error
+                raise RuntimeError("no such user")
+            _uid_cache[user] = val
+
+    if isinstance(group, str):
+        val = _gid_cache.get(group, None)
+        if val is None:
+            val = group_gid(group)
+            if val is None:
+                # TODO: nicer error
+                raise RuntimeError("no such group")
+            _gid_cache[group] = val
+
+    args = {k: v for k, v in {"uid": user, "gid": group} if v is not None}
+    if args:
+        os.chown(path, **args, follow_symlinks=False)
+    if perms:
+        os.chmod(path, perms)
+
+
+def get_paths(root_dir: Path, *patterns: str) -> Generator[Path, None, None]:
+    """Compute paths.
+
+    Args:
+        root_dir: patterns are matched relative to this directory
+        *patterns: list of strings using shell-style glob pattern syntax
+
+    NOTE:
+        * each path is only returned once, regardless of several patterns matching
+
+    Returns:
+        A generator yielding (p: Path, is_dir: bool) tuples, where p is a path
+        relative to the `root_dir` for each file/directory matching a pattern.
+    """
+    matched = set()
+    for pattern in patterns:
+        for res in glob.glob(pattern, recursive=True, root_dir=root_dir):
+            if res not in matched:
+                matched.add(res)
+                p = Path(res)
+                if (root_dir / p).is_dir():
+                    yield p, True
+                else:
+                    yield p, False
